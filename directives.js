43561a3fe1497fb919061c6c78abf3ccd3cbbe5a
Alan <alanloffler@icloud.com>
2014-07-23 19:06:21 -0300

    Added "letterInput" directive

--- directives.js
+++ directives.js
@@ -13,4 +13,19 @@ angular.module("alanloffler.directives", [])
 			});
 		}
 	};
+})
+.directive("letterInput", function() {
+	return {
+		require: "ngModel",
+		link: function(scope, elem, attrs, ctrl) {
+			ctrl.$parsers.push(function(input) {
+				var newInput = input.replace(/[0-9]/g, '');
+				if(newInput !== input) {
+					ctrl.$setViewValue(newInput);
+					ctrl.$render();
+				};
+				return newInput;
+			});
+		}
+	};
 });
