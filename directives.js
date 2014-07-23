d0d35b0c6c1a25403ba9ae3bbf839b1a96bf750f
Alan <alanloffler@icloud.com>
2014-07-23 20:03:28 -0300

    Added "phoneInput" directive
    Use numbers, () and -

--- directives.js
+++ directives.js
@@ -28,4 +28,19 @@ angular.module("alanloffler.directives", [])
 			});
 		}
 	};
+})
+.directive("phoneInput", function() {
+	return {
+		require: "ngModel",
+		link: function(scope, elem, attrs, ctrl) {
+			ctrl.$parsers.push(function(input) {
+				var newInput = input.replace(/[^0-9()-. _]/, '');
+				if(newInput !== input) {
+					ctrl.$setViewValue(newInput);
+					ctrl.$render();
+				};
+				return newInput;
+			});
+		}
+	};
 });
